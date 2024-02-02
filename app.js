7f2136818f053b049962eb6e928f826fbcda2b09
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 01:24:11 -0800

    project complete

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
 import inquirer from "inquirer";
+import showBanner from "node-banner";
+import gradient from "gradient-string";
+(async () => {
+    await showBanner("TODO LIST", "TODO LIST is used add your list forever", "red", "blue");
+})();
 let todoArray = []; // initializing empty todoArray to stor todo values
-let loop = true;
-while (loop) {
-    const answer = await inquirer.prompt([
-        {
-            type: "input",
-            name: "todo",
-            message: "What do you want to add in todo?",
-        },
-        {
-            type: "confirm",
-            name: "addmore",
-            message: "Do you want to add more todo?",
-            default: false,
-        },
-    ]);
-    const { todo, addmore } = answer;
-    loop = addmore; // when addmore will no then loop will be terminate
-    if (todo) {
-        // if user add in todo
-        todoArray.push(todo);
+async function fun() {
+    let loop = true;
+    while (loop) {
+        const answer = await inquirer.prompt([
+            {
+                type: "input",
+                name: "todo",
+                message: gradient.rainbow("What do you want to add in todo?"),
+            },
+            {
+                type: "confirm",
+                name: "addmore",
+                message: gradient.rainbow("Do you want to add more todo?"),
+            },
+        ]);
+        const { todo, addmore } = answer;
+        loop = addmore; // when addmore will no then loop will be terminate
+        if (todo) {
+            // if user add in todo
+            todoArray.push(todo);
+        }
+        else {
+            console.log(gradient.rainbow("kindly add some to do"));
+        }
+    }
+    if (todoArray.length > 0) {
+        console.log(gradient.rainbow("Your todo List"));
+        todoArray.forEach((element) => {
+            console.log(gradient.rainbow(element));
+        });
     }
     else {
-        console.log("kindly add some to do");
+        console.log(gradient.rainbow("not todo finds"));
     }
+    console.log(gradient.rainbow("thanks for using our TODO App"));
 }
-if (todoArray.length > 0) {
-    console.log("Your todo List");
-    todoArray.forEach((element) => {
-        console.log(element);
-    });
-}
-else {
-    console.log("not todo finds");
-}
+setTimeout(() => {
+    fun();
+}, 1000);

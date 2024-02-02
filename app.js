30e801c559fabab870de7f2556d4db4f5faaca5e
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 00:44:42 -0800

    project completed

---
+++ app.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+let todoArray = []; // initializing empty todoArray to stor todo values
+let loop = true;
+while (loop) {
+    const answer = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todo",
+            message: "What do you want to add in todo?",
+        },
+        {
+            type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more todo?",
+            default: false,
+        },
+    ]);
+    const { todo, addmore } = answer;
+    loop = addmore; // when addmore will no then loop will be terminate
+    if (todo) {
+        // if user add in todo
+        todoArray.push(todo);
+    }
+    else {
+        console.log("kindly add some to do");
+    }
+}
+if (todoArray.length > 0) {
+    console.log("Your todo List");
+    todoArray.forEach((element) => {
+        console.log(element);
+    });
+}
+else {
+    console.log("not todo finds");
+}
